1299dd1a1fd1661ea4df5d8eb2c484eeed07563a
Maksim Korzh <freesoft.for.people@gmail.com>
2021-07-13 16:51:37 +0300

    added routine to convert piece coordinates to FEN string

--- src/chessbot.py
+++ src/chessbot.py
@@ -14,6 +14,7 @@ import cv2
 import numpy as np
 import pyautogui as pg
 import chess
+import sys
 
 # constants (modify if needed)
 BOARD_SIZE = 400
@@ -21,7 +22,22 @@ CELL_SIZE = int(BOARD_SIZE / 8)
 BOARD_TOP_COORD = 141
 BOARD_LEFT_COORD = 5
 CONFIDENCE = 0.8
+DETECTION_NOICE_THRESHOLD = 8
 PIECES_PATH = './piece_recognition/pieces/'
+
+# players
+WHITE = 0
+BLACK = 1
+
+# side to move
+side_to_move = None
+
+# read argv if available
+try:
+    if sys.argv[1] == 'black': side_to_move = BLACK
+except:
+    print('usage: "chessbot.py white" or "chessbot.py black"')
+    sys.exit(0)
 
 # map piece names to FEN chars
 piece_names = {
@@ -58,7 +74,7 @@ def locate_piece(screenshot, piece_location):
     cv2.imshow('Screenshot', screenshot)
     cv2.waitKey(0)
 
-# convert board image to FEN string
+# get coordinates of chess pieces
 def recognize_position():
     # piece locations
     piece_locations = {
@@ -89,7 +105,8 @@ def recognize_position():
             # loop over matched pieces
             for position in piece_locations[piece]:
                 # noice detection
-                if abs(position.left - location.left) < 8 and abs(position.top - location.top) < 8:
+                if abs(position.left - location.left) < DETECTION_NOICE_THRESHOLD and \
+                   abs(position.top - location.top) < DETECTION_NOICE_THRESHOLD:
                     noise = True
                     break
             
@@ -98,10 +115,68 @@ def recognize_position():
             
             # detect piece
             piece_locations[piece].append(location)
+            print('detecting:', piece, location)
             
     # return piece locations 
     return screenshot, piece_locations
 
+# convert piece coordinates to FEN string
+def locations_to_fen(piece_locations):
+    # FEN string
+    fen = ''
+    
+    # board top left corner coords
+    x = BOARD_LEFT_COORD
+    y = BOARD_TOP_COORD
+    
+    # loop over board rows
+    for row in range(8):
+        # empty square counter
+        empty = 0
+            
+        # loop over board columns
+        for col in range(8):
+            # init square
+            square = row * 8 + col
+            
+            # piece detection
+            is_piece = ()
+            
+            # loop over piece types
+            for piece_type in piece_locations.keys():
+                # loop over pieces
+                for piece in piece_locations[piece_type]:
+                    if abs(piece.left - x) < DETECTION_NOICE_THRESHOLD and \
+                       abs(piece.top - y) < DETECTION_NOICE_THRESHOLD:
+                        if empty:
+                            fen += str(empty)
+                            empty = 0
+
+                        fen += piece_names[piece_type]
+                        is_piece = (square, piece_names[piece_type])
+            
+            if not len(is_piece):
+                empty += 1
+            
+            # increment x coord by cell size
+            x += CELL_SIZE
+        
+        if empty: fen += str(empty)
+        if row < 7: fen += '/'
+        
+        # restore x coord, increment y coordinate by cell size
+        x = BOARD_LEFT_COORD
+        y += CELL_SIZE
+    
+    # add side to move to fen
+    fen += ' ' + 'b' if side_to_move else ' w'
+    
+    # add placeholders (NO EN PASSANT AND CASTLING for now)
+    fen += ' - - 0 0'
+    
+    # return FEN string
+    return fen
+            
 
 ################################    
 #
@@ -110,13 +185,8 @@ def recognize_position():
 ################################
 
 screenshot, piece_locations = recognize_position()
-
-for piece in piece_names.keys():
-    locate_piece(screenshot, piece_locations[piece])
-
-
-
-
+fen = locations_to_fen(piece_locations)
+print('FEN:', fen)
 
 
 
